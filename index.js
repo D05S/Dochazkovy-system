fc9b9783779a7dd66c415b2b501edeae9291169a
Dominik Sauer <dominiksauer76@gmail.com>
2020-12-29 21:25:59 +0100

    add userInfo js + arrey

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 
-document.querySelector(".modal-footer .tlacitko-potvrdit").addEventListener("click", function() {   
-    console.log(document.querySelector("#personalId").value);
-} );
+
+
+function showInfo(){
+
+    var osobniCislo = document.getElementById("personalId").value;
+
+    document.getElementById("numberInfo").innerText = osobniCislo;
+
+    var jmenoP = document.getElementById("jmenoPrijimeni").value;
+
+    document.getElementById("nameInfo").innerText = jmenoP;
+
+    var department = document.getElementById("workingPlace").value;
+
+    document.getElementById("departmentInfo").innerText = department;
+    
+    var userInfo = [osobniCislo, jmenoP, department];
+
+    console.log(userInfo);
+    
+
+}
+
+
 
 
 
